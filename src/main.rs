3a194381cbdfe2372e1893a301a5fb4a801dc150
Chris Kemp <chris.kemp3000@gmail.com>
2024-02-02 13:30:12 +0000

    Pared back to minimum

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 #![no_std]
 #![no_main]
 
-use core::time::Duration;
+// use core::time::Duration;
 
 use panic_halt as _;
 
@@ -9,70 +9,74 @@ use arduino_nano33iot as bsp;
 use bsp::entry;
 use bsp::hal;
 use hal::clock::GenericClockController;
-use hal::delay::Delay;
-use hal::pac::{CorePeripherals, Peripherals};
+// use hal::delay::Delay;
+use hal::pac::{
+    // CorePeripherals,
+    Peripherals
+};
 use hal::prelude::*;
 
-mod gyro;
-mod orientation;
+// mod gyro;
+// mod orientation;
 mod time;
-mod usb;
-use usb::usb_log;
-mod wifi;
+// mod usb;
+// use usb::usb_log;
+// mod wifi;
 
 #[entry]
 fn main() -> ! {
     let mut peripherals = Peripherals::take().unwrap();
-    let core = CorePeripherals::take().unwrap();
+    let pins = bsp::Pins::new(peripherals.PORT);
+    let mut led: bsp::Led = pins.led_sck.into();
+
+    // let core = CorePeripherals::take().unwrap();
     let mut clocks = GenericClockController::with_internal_32kosc(
         peripherals.GCLK,
         &mut peripherals.PM,
         &mut peripherals.SYSCTRL,
         &mut peripherals.NVMCTRL,
     );
-    let mut delay = Delay::new(core.SYST, &mut clocks);
-    let pins = bsp::Pins::new(peripherals.PORT);
-    let mut led: bsp::Led = pins.led_sck.into();
+    // let mut delay = Delay::new(core.SYST, &mut clocks);
     let gclk0 = clocks.gclk0();
     let mut timer = time::Timer::new(peripherals.TC5, &mut clocks, &gclk0, &mut peripherals.PM);
 
-    let mut gyro = gyro::setup_gyro(
-        &mut clocks,
-        peripherals.SERCOM4,
-        &mut peripherals.PM,
-        pins.sda,
-        pins.scl,
-    )
-    .unwrap();
+    // let mut gyro = gyro::setup_gyro(
+    //     &mut clocks,
+    //     peripherals.SERCOM4,
+    //     &mut peripherals.PM,
+    //     pins.sda,
+    //     pins.scl,
+    // )
+    // .unwrap();
 
-    unsafe {
-        usb::setup_usb(
-            &mut clocks,
-            peripherals.USB,
-            &mut peripherals.PM,
-            pins.usb_dm,
-            pins.usb_dp,
-            // &mut core,
-        );
-    }
+    // unsafe {
+    //     usb::setup_usb(
+    //         &mut clocks,
+    //         peripherals.USB,
+    //         &mut peripherals.PM,
+    //         pins.usb_dm,
+    //         pins.usb_dp,
+    //         // &mut core,
+    //     );
+    // }
 
-    let clock = clocks.sercom2_core(&gclk0).unwrap();
+    // let clock = clocks.sercom2_core(&gclk0).unwrap();
 
-    let wifi_pins = wifi::WifiPins {
-        miso: pins.nina_miso,
-        mosi: pins.nina_mosi,
-        sck: pins.nina_sck,
-        ack: pins.nina_ack,
-        rst: pins.nina_resetn,
-        cs: pins.nina_cs,
-    };
-    let _wifi = wifi::setup_wifi(
-        &clock,
-        &peripherals.PM,
-        peripherals.SERCOM2,
-        wifi_pins,
-        |d: Duration| delay.delay_ms(d.as_millis() as u32),
-    );
+    // let wifi_pins = wifi::WifiPins {
+    //     miso: pins.nina_miso,
+    //     mosi: pins.nina_mosi,
+    //     sck: pins.nina_sck,
+    //     ack: pins.nina_ack,
+    //     rst: pins.nina_resetn,
+    //     cs: pins.nina_cs,
+    // };
+    // // let _wifi = wifi::setup_wifi(
+    //     &clock,
+    //     &peripherals.PM,
+    //     peripherals.SERCOM2,
+    //     wifi_pins,
+    //     |d: Duration| delay.delay_ms(d.as_millis() as u32),
+    // );
 
     // match wifi.scan_networks() {
     //     Ok(networks) => {
@@ -93,55 +97,55 @@ fn main() -> ! {
     loop {
         timer.tick();
         let now = timer.millis();
-        if 500 < (now - led_last_toggled) {
+        if (now - led_last_toggled) > 100 {
             led.toggle().unwrap();
             led_last_toggled = now;
         }
-        unsafe {
-            // NB Must be called at least once every 10ms to stay
-            // USB-compliant.
-            poll_usb(&timer, &mut gyro);
-        }
+    //     unsafe {
+    //         // NB Must be called at least once every 10ms to stay
+    //         // USB-compliant.
+    //         poll_usb(&timer, &mut gyro);
+    //     }
     }
 }
 
-unsafe fn poll_usb(timer: &time::Timer, gyro: &mut gyro::Gyro) {
-    if let Some(usb_dev) = usb::USB_BUS.as_mut() {
-        if let Some(serial) = usb::USB_SERIAL.as_mut() {
-            usb_dev.poll(&mut [serial]);
+// unsafe fn poll_usb(timer: &time::Timer, gyro: &mut gyro::Gyro) {
+//     if let Some(usb_dev) = usb::USB_BUS.as_mut() {
+//         if let Some(serial) = usb::USB_SERIAL.as_mut() {
+//             usb_dev.poll(&mut [serial]);
 
-            let mut buf = [0u8; 127];
-            if let Ok(count) = serial.read(&mut buf) {
-                handle_serial(
-                    core::str::from_utf8(&buf[..count - 1]).unwrap(),
-                    timer,
-                    gyro,
-                );
-            }
-        }
-    };
-}
+//             let mut buf = [0u8; 127];
+//             if let Ok(count) = serial.read(&mut buf) {
+//                 handle_serial(
+//                     core::str::from_utf8(&buf[..count - 1]).unwrap(),
+//                     timer,
+//                     gyro,
+//                 );
+//             }
+//         }
+//     };
+// }
 
-fn handle_serial(s: &str, timer: &time::Timer, gyro: &mut gyro::Gyro) {
-    match s {
-        "ping" => usb_log("pong\n"),
-        "tick" => {
-            let mut buf = [0u8; 32];
-            usb_log("rtc: ");
-            lexical_core::write(timer.millis(), &mut buf);
-            usb_log(core::str::from_utf8(&buf).unwrap());
-            usb_log("\n");
-        }
-        "gyro" => log_gyro(gyro),
-        _ => usb_log("unknown command\n"),
-    };
-}
+// fn handle_serial(s: &str, timer: &time::Timer, gyro: &mut gyro::Gyro) {
+//     match s {
+//         "ping" => usb_log("pong\n"),
+//         "tick" => {
+//             let mut buf = [0u8; 32];
+//             usb_log("rtc: ");
+//             lexical_core::write(timer.millis(), &mut buf);
+//             usb_log(core::str::from_utf8(&buf).unwrap());
+//             usb_log("\n");
+//         }
+//         "gyro" => log_gyro(gyro),
+//         _ => usb_log("unknown command\n"),
+//     };
+// }
 
-fn log_gyro(gyro: &mut gyro::Gyro) {
-    if let Ok((gx, gy, gz)) = gyro.read_accelerometer() {
-        usb_log(orientation::display_orientation(orientation::orientation(
-            gx, gy, gz,
-        )));
-        usb_log("\n");
-    }
-}
+// fn log_gyro(gyro: &mut gyro::Gyro) {
+//     if let Ok((gx, gy, gz)) = gyro.read_accelerometer() {
+//         usb_log(orientation::display_orientation(orientation::orientation(
+//             gx, gy, gz,
+//         )));
+//         usb_log("\n");
+//     }
+// }
